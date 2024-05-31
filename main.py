e1e8f1f065397757cf1e633439c318cf6d846746
Bob Lin <boblinlj@gmail.com>
2024-05-30 22:18:17 -0400

    added sql client

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sec_api_job import sec_api
 from sec_api_job import sec_pop
 from sec_api_job import  data_processor
+import os
 
 # sec_client = sec_api.SecApiClient('boblinlj@gmail.com')
 # cik_list = sec_pop.get_sec_ticker_mapping()
@@ -9,6 +10,21 @@ from sec_api_job import  data_processor
 #     result = sec_client.get_factor(cik)
 #     sec_api.create_staging_file(result,cik)
 
-file_processor = data_processor.SecFileProcessor()
+file_processor = data_processor.SecFileProcessor('2024-05-31')
 
-file_processor.flat_dict('staging_data/0001944977_factor_2024-05-29.txt')
+sql_client = data_processor.MySqlClient(database_ip='127.0.0.1',
+                                        database_port='3306',
+                                        database_user='root',
+                                        database_pw='Bob060443',
+                                        database_name='sec')
+
+# df = file_processor.flat_dict('staging_data/0001941536_factor_2024-05-29.txt')
+
+# sql_client.insert_df(df, False, 'sec_facts')
+
+directory = os.fsencode('staging_data')
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    df = file_processor.flat_dict(os.path.join('staging_data',filename))
+    sql_client.insert_df(df, False, 'sec_facts')
--- sec_api_job/data_processor.py
+++ sec_api_job/data_processor.py
@@ -3,14 +3,18 @@ import json
 from datetime import date
 from sec_api_job import util
 import pandas as pd
+from sqlalchemy import create_engine
 
 
-def create_staging_file(self, json, cik):
+def create_staging_file(json, cik):
         file=os.path.join('staging_data',f'{cik}_factor_{date.today()}.txt')
         util.write_text_file(json, file)
         return None
 
 class SecFileProcessor():
+    
+    def __init__(self, updated_dt) -> None:
+        self.updated_dt =  updated_dt
     
     def _read_file_to_json(self, file_name) -> dict:
         f = open(file_name, 'r')
@@ -25,24 +29,56 @@ class SecFileProcessor():
         json_data = self._read_file_to_json(file_name)
         # if json_data is None, return an empty dataframe
         if json_data is None: return pd.DataFrame()
-        
         cik = json_data['cik']
         facts = json_data['facts']
         stock_data = []
         for fact in facts:
-            for item in json_data['facts'][fact]:
-                for unit in json_data['facts'][fact][item]['units']:
-                    data = json_data['facts'][fact][item]['units'][unit]
+            for taxonomy in json_data['facts'][fact]:
+                for unit in json_data['facts'][fact][taxonomy]['units']:
+                    data = json_data['facts'][fact][taxonomy]['units'][unit]
                     df = pd.DataFrame.from_dict(data)
                     df['unit'] = unit
-                    df['item'] = item
+                    df['taxonomy'] = taxonomy
                     df['fact'] = fact
                     df['cik'] = cik
+                    df['updated_date']= self.updated_dt
                     stock_data.append(df)
         
         if len(stock_data) > 0:
             stock_df = pd.concat(stock_data, axis=0)
+            stock_df.to_csv('check.csv',index=False)
             return stock_df
         else:
             return pd.DataFrame()
 
+class MySqlClient():
+    def __init__(self, 
+                 database_ip,
+                 database_port,
+                 database_user,
+                 database_pw,
+                 database_name
+                 ) -> None:
+        self.database_ip = database_ip
+        self.database_port = database_port
+        self.database_user = database_user
+        self.database_pw = database_pw
+        self.database_name = database_name
+        
+        self.cnn = create_engine(
+                    f"""mysql+mysqlconnector://{self.database_user}"""
+                    f""":{self.database_pw}"""
+                    f"""@{self.database_ip}"""
+                    f""":{self.database_port}"""
+                    f"""/{self.database_name}""",
+                    pool_size=20,
+                    max_overflow=0
+                    )
+    
+    def insert_df(self, df, insert_index, table):
+        df.to_sql(name=table,
+                  con=self.cnn,
+                  if_exists='append',
+                  index=insert_index,
+                  method='multi',
+                  chunksize=1)
