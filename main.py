5f60e75508ebba8d882acd2b1abff9f4c9a81010
Bob Lin <boblinlj@gmail.com>
2024-06-02 14:07:07 -0400

    added sec rss client

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from sec_api_job import util
 import os
 import time
 
+import feedparser
+feed = feedparser.parse('https://www.sec.gov/Archives/edgar/xbrl-inline.rss.xml')
+print(feed)
+
 # sec_client = sec_api.SecApiClient('boblinlj@gmail.com')
-# cik_list = sec_pop.get_sec_ticker_mapping()
 
 # for cik in cik_list:
 #     result = sec_client.get_factor(cik)
@@ -18,26 +21,27 @@ import time
 
 # sql_client.insert_df(df, False, 'sec_facts')
 
-def run_job(filename):
-    file_processor = data_processor.SecFileProcessor('2024-05-31')
+# def run_job(filename):
+#     file_processor = data_processor.SecFileProcessor('2024-05-31')
 
-    sql_client = data_processor.MySqlClient(database_ip='127.0.0.1',
-                                            database_port='3306',
-                                            database_user='root',
-                                            database_pw='Bob060443',
-                                            database_name='sec')
-    start = time.time()
-    filename = os.fsdecode(filename)
-    df = file_processor.flat_dict(os.path.join('staging_data',filename))
-    finish1 = time.time()
-    sql_client.insert_df(df, False, 'sec_facts')
-    finish2 = time.time()
+#     sql_client = data_processor.MySqlClient(database_ip='127.0.0.1',
+#                                             database_port='3306',
+#                                             database_user='root',
+#                                             database_pw='Bob060443',
+#                                             database_name='sec')
+#     start = time.time()
+#     filename = os.fsdecode(filename)
+#     df = file_processor.flat_dict(os.path.join('staging_data',filename))
+#     finish1 = time.time()
+#     sql_client.insert_df(df, False, 'sec_facts')
+#     finish2 = time.time()
     
-    print(f"{filename} - {finish1-start} - {finish2 - finish1}")
+#     print(f"{filename} - {finish1-start} - {finish2 - finish1}")
 
-directory = os.fsencode('staging_data')
-# f = open('timer.log', 'a')
-file_list = os.listdir(directory)
-util.parallel_process(file_list, run_job, n_jobs=30, front_num=3, use_tqdm=True)
+# directory = os.fsencode('staging_data')
+# # f = open('timer.log', 'a')
+# file_list = os.listdir(directory)
+# util.parallel_process(file_list, run_job, n_jobs=30, front_num=3, use_tqdm=True)
     
 # f.close()
+
--- sec_api_job/sec_api.py
+++ sec_api_job/sec_api.py
@@ -1,9 +1,7 @@
 import requests
-import pathlib
-import os
 import pandas as pd
 from pyrate_limiter import Duration, Limiter, Rate
-from datetime import date
+import xmltodict
 
 # 10 requests per second rate limit set by SEC:
 # https://www.sec.gov/os/webmaster-faq#developers
@@ -51,6 +49,33 @@ class SecApiClient():
         url = self.request_api(cik)
         return self.request_api(url)
 
+class SecRSSClient():
+    def fetch_filings_from_rss(self, url):
+        exit_data = dict()
+        s = requests.Session()
+        s.headers.update({'User-Agent': 'Mozilla/5.0'})
+        r = s.get(url)
+        feed = xmltodict.parse(r.content)
+        pubDate = feed['rss']['channel']['pubDate'].replace(" EDT","")
+        title =  feed['rss']['channel']['title']
+        for item in feed['rss']['channel']['item']:
+            formType = item['edgar:xbrlFiling'].get('edgar:formType')
+            cikNumber = item['edgar:xbrlFiling'].get('edgar:cikNumber')
+            companyName = item['edgar:xbrlFiling'].get('edgar:companyName')
+            filingDate = item['edgar:xbrlFiling'].get('edgar:filingDate')
+            fiscalYearEnd = item['edgar:xbrlFiling'].get('edgar:fiscalYearEnd')
+            exit_data[cikNumber]=[formType, pubDate, title, companyName, filingDate, fiscalYearEnd]
+
+        df = pd.DataFrame.from_dict(exit_data, 
+                                    orient='index',
+                                    columns=['formType', 'pubDate', 'title', 'companyName', 'filingDate', 'fiscalYearEnd']
+                                    )
+        df.reset_index(inplace=True)
+        df.rename(columns={'index':'cik'},inplace=True)
+        df['pubDate'] = pd.to_datetime(df['pubDate'])
+        df['filingDate'] = pd.to_datetime(df['filingDate'])
+        
+        return df
 
 if __name__ == '__main__':
     pass
